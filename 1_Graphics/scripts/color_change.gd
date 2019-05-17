cfab2af7f762ecce6c5ed39cd796ba431f1472eb
taylus <therealbjohnson@gmail.com>
2019-05-16 21:17:26 -0500

    Use a timer instead

--- 1_Graphics/scripts/color_change.gd
+++ 1_Graphics/scripts/color_change.gd
@@ -1,13 +1,17 @@
 extends Sprite
 
 export var colorChangeMs = 300    # change colors every this many milliseconds
-var lastChangedTicks: int
+var timer: Timer
 
-func _process(delta) -> void:
-	var ticks = OS.get_ticks_msec()
-	if (ticks - lastChangedTicks > colorChangeMs):
-		lastChangedTicks = ticks
-		modulate = random_color()
+func _ready() -> void:
+	timer = Timer.new()
+	timer.connect("timeout", self, "tick")
+	timer.wait_time = colorChangeMs / 1000.0
+	timer.start()
+	add_child(timer)
+	
+func tick():
+	modulate = random_color()
 	
 func random_color() -> Color:
 	return Color(rand_range(0, 1), rand_range(0, 1), rand_range(0, 1))
